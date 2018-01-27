b209fc4d8b782fb4e41cb4e0d8930cc225b3ead6
Eyüp Gürel <eyupgurel@gmail.com>
2018-01-27 16:17:43 +0300

    New files

---
+++ app/src/main/java/com/alienstar/cyrus/recipe/ui/recipe/RecipeContract.java
@@ -0,0 +1,14 @@
+package com.alienstar.cyrus.recipe.ui.recipe;
+
+/**
+ * Created by cyrus on 1/27/18.
+ */
+
+public interface RecipeContract {
+    interface View {
+
+    }
+    interface Listener {
+        
+    }
+}
